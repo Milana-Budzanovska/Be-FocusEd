fa87aae533e8902ce1178c9f51cf7a800b961abf
Milana-Budzanovska <budzanovskamilana@gmail.com>
2025-04-10 01:32:35 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MoodCheck from './MoodCheck'; // Створення вибору настрою
-import MainMenu from './MainMenu';  // Головне меню
-import Learning from './Learning';  // Сторінка навчання
-import Breathing from './Breathing';  // Дихальні вправи
-import Progress from './Progress';  // Прогрес
-import Settings from './Settings';  // Налаштування
+import MoodCheck from './MoodCheck';
+import MainMenu from './MainMenu';
+import Learning from './Learning';
+import Breathing from './Breathing';
+import Progress from './Progress';
+import Settings from './Settings';
+import Forum from './pages/Forum'; // Імпортуємо Forum
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Додано маршрут для вибору настрою */}
         <Route path="/" element={<MoodCheck />} />
-        
-        {/* Основні маршрути для меню та інших компонентів */}
         <Route path="/menu" element={<MainMenu />} />
         <Route path="/learning" element={<Learning />} />
         <Route path="/breathing" element={<Breathing />} />
         <Route path="/progress" element={<Progress />} />
         <Route path="/settings" element={<Settings />} />
-        
-        {/* Фолбек для неправильних маршрутів */}
+        <Route path="/forum" element={<Forum />} /> {/* ➕ Маршрут для форуму */}
         <Route path="*" element={<MainMenu />} />
       </Routes>
     </Router>
