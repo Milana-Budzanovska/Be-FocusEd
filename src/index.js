700a3ed4e99c6efdaa3ce1237bd5036f2f11079d
Milana-Budzanovska <budzanovskamilana@gmail.com>
2025-04-10 02:07:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,30 @@
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import MoodCheck from './MoodCheck';
 import MainMenu from './MainMenu';
-import Breathing from './Breathing';
 import Learning from './Learning';
+import Breathing from './Breathing';
 import Progress from './Progress';
 import Settings from './Settings';
-import Forum from './Forum'; 
-
-import './App.css';
-import './Forum.css';
-
-const Root = () => {
-  return (
-    <Router>
-     <Routes>
-  <Route path="/" element={<MoodCheck />} />
-  <Route path="/menu" element={<MainMenu />} />
-  <Route path="/learning" element={<Learning />} />
-  <Route path="/breathing" element={<Breathing />} />
-  <Route path="/progress" element={<Progress />} />
-  <Route path="/settings" element={<Settings />} />
-  <Route path="/forum" element={<Forum />} /> {/* ✅ Додано */}
-  <Route path="*" element={<MainMenu />} />
-     </Routes>
-    </Router>
-  );
-};
+import Forum from './forum'; // імпортуємо сторінку форуму
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Root />);
-
-// ✅ Активуємо Service Worker для роботи PWA
-serviceWorkerRegistration.register();
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MoodCheck />} />
+        <Route path="/menu" element={<MainMenu />} />
+        <Route path="/learning" element={<Learning />} />
+        <Route path="/breathing" element={<Breathing />} />
+        <Route path="/progress" element={<Progress />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/forum" element={<Forum />} /> {/* ✅ Доданий маршрут */}
+        <Route path="*" element={<MainMenu />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
