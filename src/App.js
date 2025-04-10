ddfa568202f5d4db2810e7cd450e957fecdfeb11
Milana-Budzanovska <budzanovskamilana@gmail.com>
2025-04-11 00:55:26 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MoodCheck from './MoodCheck';
@@ -6,19 +7,26 @@ import Learning from './Learning';
 import Breathing from './Breathing';
 import Progress from './Progress';
 import Settings from './Settings';
-import Forum from './Forum'; // <-- додано
+import Forum from './Forum'; // ✅ Додано імпорт форуму
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* 🌤️ Початкова сторінка – вибір настрою */}
         <Route path="/" element={<MoodCheck />} />
+
+        {/* 📚 Основні сторінки */}
         <Route path="/menu" element={<MainMenu />} />
         <Route path="/learning" element={<Learning />} />
         <Route path="/breathing" element={<Breathing />} />
         <Route path="/progress" element={<Progress />} />
         <Route path="/settings" element={<Settings />} />
-        <Route path="/forum" element={<Forum />} /> {/* <-- Ось цей маршрут */}
+
+        {/* 🧑‍🤝‍🧑 Форум / спільнота */}
+        <Route path="/forum" element={<Forum />} />
+
+        {/* 🛑 Фолбек: якщо неправильний маршрут */}
         <Route path="*" element={<MainMenu />} />
       </Routes>
     </Router>
